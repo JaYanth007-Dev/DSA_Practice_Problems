3ecba99825719391bfa03dec4719223adfbff166
Jayanth Kumar <jayanthchalla12345@gmail.com>
2025-11-06 23:29:20 +0530

    Number pattern Added

--- Js/NumberPatterns.js
+++ Js/NumberPatterns.js
@@ -14,3 +14,21 @@ for (let index = 1; index <= 5; index++) {
   console.log(row);
 }
 
+let n=10
+for (let index = 1; index <= n; index++) {
+    let row = " ";
+
+    for (let j = 1; j <= n; j++) {
+        if (index + j <= n+1) {
+            row = row + " " + j;
+        }
+        else {
+            row = row + " ";
+        }
+    }
+  
+  console.log(row);
+}
+
+
+
